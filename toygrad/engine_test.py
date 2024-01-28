614cb776effd5fb43d52623dcd83713258429828
Kevin Hu <hxy9243@gmail.com>
2024-01-27 22:53:57 -0800

    add test

--- toygrad/engine_test.py
+++ toygrad/engine_test.py
@@ -5,7 +5,8 @@ def test_basic_value():
     x = Value(3.0)
     y = Value(6.0)
 
-    assert (x + 3) * 2 == Value(12.0)
-    assert (3 * x) + 5 == Value(14.0)
-    assert x * y + y * 2 == Value(30.0)
-    assert x**2 / y == Value(1.5)
+    assert ((x + 3) * 2).val == 12.0
+    assert ((3 * x) + 5).val == 14.0
+    assert (x * y + y * 2).val == 30.0
+    assert (x**2 / y).val == 1.5
+    assert (x**2 - y).val == 3.0
